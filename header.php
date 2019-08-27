731134b58c8ff9bda001a3d375ec347aaaef2fc7
Paulius911 <48103462+Paulius911@users.noreply.github.com>
2019-08-27 15:40:51 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Namu darbas 8</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css?v=2" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&amp;subset=latin-ext" rel="stylesheet">
+</head>
+
+    <body>
+        <nav>
+          <div class="topnav">
+            <div class="rightnav">
+              <h1>Back To Basics</h1>
+              <p>Free PSD Website Templates</p>
+              </div>
+            </div>
+            <div class="leftnav">
+              <input type="text" placeholder="Search our website">
+              <button type="button" name="Search">Search</button>
+            </div>
+        </nav>
+        <nav class="bottomnav">
+          <div class="rightnavbottom">
+            <a>Home</a>
+            <a>Style DEMO</a>
+            <a>full width</a>
+            <a>drop down</a>
+            <a>portfolio</a>
+            <a>gallary</a>
+          </div>
+          <div class="leftnavbottom">
+            <img src="img/in.png">
+            <img src="img/twitter.png">
+            <img src="img/pinblack.png">
+            <img src="img/google.png">
+            <img src="img/wifi.png">
+          </div>
+        </nav>
+      </div>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php include 'header.php';  ?>
+
+  <section>
+    <div class="greystuff">
+      <div class="paragraph">
+        <p>
+        Sint minim aliqua voluptate elit reprehenderit aliquip et Lorem excepteur. Ipsum ex elit nostrud . Ipsum ex elit nostrud
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="thirds">
+    <div class="first">
+      <h1>YOUR TITLE HERE</h1>
+      <p>Laboris consectetur aliquip incididunt consectetur non deserunt commodo nulla elit culpa sit. Aute aute proident culpa dolor minim proident elit consectetur nostrud fugiat Lorem consectetur Lorem minim irure. </p>
+      <button type="button" name="button">READ MORE</button>
+    </div>
+    <div class="second">
+      <h1>YOUR TITLE HERE</h1>
+      <p>Laboris consectetur aliquip incididunt consectetur non deserunt commodo nulla elit culpa sit. Aute aute proident culpa dolor minim proident elit consectetur nostrud fugiat Lorem consectetur Lorem minim irure..</p>
+      <button type="button" name="button">READ MORE</button>
+    </div>
+    <div class="third">
+      <h1>YOUR TITLE HERE</h1>
+      <p>Laboris consectetur aliquip incididunt consectetur non deserunt commodo nulla elit culpa sit. Aute aute proident culpa dolor minim proident elit consectetur nostrud fugiat Lorem consectetur Lorem minim irure..</p>
+      <button type="button" name="button">READ MORE</button>
+    </div>
+  </section>
+
+  <section class="twocolumns">
+    <div class="rightcolumnn">
+      <h1>SOME OUR SERVICES</h1>
+        <div>
+          <img src="img/StockSnap.jpg">
+          <h2>Title Goes Here</h2>
+          <p>Anim commodo pariatur elit in irure cillum mollit velit nostrud ipsum ut aliquip dolore dot Lorem consectetur Lorem minim irure..</p>
+        </div>
+        <div>
+          <img src="img/StockSnap.jpg">
+          <h2>Title Goes Here</h2>
+          <p>Consequat sit nostrud reprehenderit sit tempor do nisi elit. t Lorem consectetur Lorem minim irure.</p>
+        </div>
+        <div>
+          <img src="img/StockSnap.jpg">
+          <h2>Title Goes Here</h2>
+          <p>Quis laboris dolore eiusmod ad ipsum pariatur culpa veniam Lorem ut pro t Lorem consectetur Lorem minim irure. ident nisi eu aliqua.</p>
+        </div>
+        <div>
+          <img src="img/StockSnap.jpg">
+          <h2>Title Goes Here</h2>
+          <p>Elit voluptate ipsum est t Lorem consectetur Lorem minim irure. laborum labore pariatur irure.</p>
+        </div>
+    </div>
+    <div class="leftcolumn">
+      <h1>A LITTLE ABOUT US</h1>
+      <img src="img/StockSnap.jpg">
+       <p class="parag">
+         Laborum fugiat consequat reprehenderit sunt ad Lorem Lorem minim eiusmod aliquip in.Ea qui tempor non eiusmod qui esse amet laboris occaecat consequat proident amet aliquip elit sunt sit pariatur.
+       </p>
+       <p>
+       Duis veniam ex nisi exercitation consequat culpa reprehenderit sunt commodo qui sint quis fugiat.Pariatur id laboris sint aute irure sit excepteur pariatur ut ea deserunt.
+     </p>
+       <button type="button" name="button">READ MORE</button>
+       <h1 class="h1">LAtest testimontial</h1>
+       <img src="img/quotation-marks.svg" class="quatation">
+       <h2>
+         <b>Nisi sunt excepteur</b>-CEO
+       </h2>
+       <p>consequat Lorem proident</p>
+       <p>
+         Aute ullamco ut ut mollit pariatur tempor adipisicing enim do officia sunt qui labore adipisicing aliquip. Non proident exercitation enim sunt veniam nulla enim incididunt est ut mollit excepteur aliquip ut mollit excepteur eiusmod. Reprehenderit mollit aliqua incididuIn mollit ut officia mollit aute incididunt id ex esse.
+       </p>
+    </div>
+  </section>
+<?php include 'footer.php'; ?>
